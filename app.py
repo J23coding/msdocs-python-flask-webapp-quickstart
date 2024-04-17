3d1c6e2ba4b209beabc3edfebe080aed0bde52b9
J23coding <167088652+J23coding@users.noreply.github.com>
2024-04-17 20:50:32 +1000

    Test app.py

--- app.py
+++ app.py
@@ -1,4 +1,7 @@
 import os
+import urllib.request
+import json
+import ssl
 
 from flask import (Flask, redirect, render_template, request,
                    send_from_directory, url_for)
@@ -18,14 +21,51 @@ def favicon():
 
 @app.route('/hello', methods=['POST'])
 def hello():
-   name = request.form.get('name')
+   temp = request.form.get('temp')
 
-   if name:
-       print('Request for hello page received with name=%s' % name)
-       return render_template('hello.html', name = name)
-   else:
-       print('Request for hello page received with no name or blank name -- redirecting')
-       return redirect(url_for('index'))
+temp = int(temp)/100
+
+data =   {
+   "Inputs": { 
+     "data": [
+       {
+         "day": 13,
+         "mnth": 3,   
+         "year": 2023,
+         "season": 1,
+         "holiday": 0,
+         "weekday": 0,
+         "workingday": 1,
+         "weathersit": 6, 
+         "temp": temp, 
+         "atemp": 0.1,
+         "hum": 0.5,
+         "windspeed": 0.3
+       }
+     ]    
+   },   
+   "GlobalParameters": 1.0
+ }
+
+body = str.encode(json.dumps(data))
+
+url = 'http://54dabd99-b27c-4ffd-9394-e2ababbf499c.australiasoutheast.azurecontainer.io/score'
+# Replace this with the primary/secondary key or AMLToken for the endpoint
+api_key = 'e6H0YTbuQdcVxzBvMebO35YpS9bZJaSx'
+
+headers = {'Content-Type':'application/json', 'Authorization':('Bearer '+ api_key)}
+
+req = urllib.request.Request(url, body, headers)
+
+response = urllib.request.urlopen(req)
+result = response.read()
+  
+if result:
+   print('Request for hello page received with name=%s' % result)
+   return render_template('hello.html', name = result)
+else:
+   print('Request for hello page received with no name or blank name -- redirecting')
+   return redirect(url_for('index'))
 
 
 if __name__ == '__main__':
