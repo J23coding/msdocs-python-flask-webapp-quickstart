fda73bcd5f91e8d3c078b752252186143e4b90e0
J23coding <167088652+J23coding@users.noreply.github.com>
2024-04-17 21:49:04 +1000

    Update app.py

--- app.py
+++ app.py
@@ -51,10 +51,13 @@ def hello():
     req = urllib.request.Request(url, body, headers)
     response = urllib.request.urlopen(req)
     result = response.read()
-  
+    ttt = result.decode('utf-8')
+    t1 = ttt.find("[")
+    t2 = ttt.find("]")
+    ttt3 = ttt[t1+1:t2]
     if result:
-       # print('Request for hello page received with name=%s' % result)
-       return render_template('hello.html', name = result)
+       print('Request for hello page received with name=%s' % ttt3)
+       return render_template('hello.html', name = ttt3)
     else:
        print('Request for hello page received with no name or blank name -- redirecting')
        return redirect(url_for('index'))
