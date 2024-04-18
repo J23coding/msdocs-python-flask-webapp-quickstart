8f603576a49d8652c22999cbf4700d07b8be05db
J23coding <167088652+J23coding@users.noreply.github.com>
2024-04-18 19:49:03 +1000

    Update app.py

--- app.py
+++ app.py
@@ -44,22 +44,22 @@ def hello():
          },   
        "GlobalParameters": 1.0
       }
-    body = str.encode(json.dumps(data)
-    url = 'http://54dabd99-b27c-4ffd-9394-e2ababbf499c.australiasoutheast.azurecontainer.io/score'
-    api_key = 'e6H0YTbuQdcVxzBvMebO35YpS9bZJaSx'
-    headers = {'Content-Type':'application/json', 'Authorization':('Bearer '+ api_key)}
-    req = urllib.request.Request(url, body, headers)
-    response = urllib.request.urlopen(req)
-    result = response.read()
-    ttt = result.decode('utf-8')
-    t1 = ttt.find("[")
-    t2 = ttt.find("]")
-    ttt3 = ttt[t1+1:t2]
-    # print('Request for hello page received with name=%s' % ttt3)
-    return render_template('hello.html', name = ttt3)
-    # else:
-    #   print('Request for hello page received with no name or blank name -- redirecting')
-    #   return redirect(url_for('index'))
+  body = str.encode(json.dumps(data))
+  url = 'http://54dabd99-b27c-4ffd-9394-e2ababbf499c.australiasoutheast.azurecontainer.io/score'
+  api_key = 'e6H0YTbuQdcVxzBvMebO35YpS9bZJaSx'
+  headers = {'Content-Type':'application/json', 'Authorization':('Bearer '+ api_key)}
+  req = urllib.request.Request(url, body, headers)
+  response = urllib.request.urlopen(req)
+  result = response.read()
+  ttt = result.decode('utf-8')
+  t1 = ttt.find("[")
+  t2 = ttt.find("]")
+  ttt3 = ttt[t1+1:t2]
+  print('Request for hello page received with name=%s' % ttt3)
+  return render_template('hello.html', result = ttt3)
+  # else:
+  #   print('Request for hello page received with no name or blank name -- redirecting')
+  #   return redirect(url_for('index'))
 
 
 if __name__ == '__main__':
